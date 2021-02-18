a877c88d0741a82f676c881a8311da50c5b830e1
wangwangla <2818815189@qq.com>
2021-02-18 20:48:32 +0800

    增加笔记

---
+++ unitycode/unitycode/Shooter1/Assets/script/Buttlet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttlet : MonoBehaviour
+{
+    Rigidbody body;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = Vector3.forward * 10;
+    }
+}
